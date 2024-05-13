bdc2b3231ac53a344f9afe8d4138179eaee5c8f7
Shantanu143 <shantanunirapal@gmail.com>
2024-05-13 08:03:22 +0530

    navbar added for rahul

--- src/AceternityUI/ui/TextGenerateEffect.tsx
+++ src/AceternityUI/ui/TextGenerateEffect.tsx
@@ -44,7 +44,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-bold", className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black text-lg leading-snug tracking-wide">
+        <div className=" dark:text-white text-black text-sm leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
