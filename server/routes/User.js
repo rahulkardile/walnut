74a44c73a03cd590a78379201b508875a7d1bc59
Rahul kardile <rahulkardile321@gmail.com>
2024-05-14 20:51:31 +0530

    User login route is updated

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,9 +1,32 @@
 import express from "express"
+import User from "../model/User.js";
+import bcrypt from "bcrypt"
 
 const router = express.Router();
 
-router.get("/get", async(req, res, next)=>{
-    res.send("user got")
+router.post("/new", async (req, res, next) => {
+    try {
+
+        const { name, email, ProfileIMG, DOB, gender, password } = req.body;
+
+        if (!name, !email, !password, !DOB, !gender) return next(400, "bad req!");
+
+        const hash = bcrypt.hashSync(password, 10)
+
+        const NewUser = await User.create({
+            name,
+            email,
+            ProfileIMG,
+            gender,
+            DOB,
+            password: hash
+        })
+
+        res.status(200).json(`Welcome ${NewUser.name}`)
+
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default router
