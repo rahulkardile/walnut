c626a980221a80512342137d605e8ca190cc5eea
Rahul kardile <rahulkardile321@gmail.com>
2024-05-15 03:07:43 +0530

    Login is updated with cookies

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,9 +1,12 @@
 import express from "express"
 import User from "../model/User.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 import { ErrorHandler } from "../utils/ErrorHandler.js";
 
 const router = express.Router();
+
+const expiry = 1000 * 60 * 60 * 24 * 300;
 
 router.post("/new", async (req, res, next) => {
     try {
@@ -54,7 +57,9 @@ router.post("/get", async (req, res, next) => {
 
         const { password: pass, createdAt, updatedAt, __v, ...rest } = GetUser._doc
 
-        res.status(200).json({
+        const access_user = jwt.sign({_id: rest._id, email: rest.email, role: rest.role}, process.env.JWT_SECRET);
+
+        res.cookie("walnut_user", access_user, {secure: true, maxAge: expiry}).status(200).json({
             message: `Welcome Back ${GetUser.name}`,
             data: rest
         })
