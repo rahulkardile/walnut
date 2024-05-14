e0756484d3edcb324941b6276bd66b56a9dce74e
Rahul kardile <rahulkardile321@gmail.com>
2024-05-15 02:57:25 +0530

    Login added to backend

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,6 +1,7 @@
 import express from "express"
 import User from "../model/User.js";
 import bcrypt from "bcrypt"
+import { ErrorHandler } from "../utils/ErrorHandler.js";
 
 const router = express.Router();
 
@@ -28,5 +29,40 @@ router.post("/new", async (req, res, next) => {
         next(error)
     }
 })
+
+router.post("/get", async (req, res, next) => {
+    try {
+
+        const { email, password } = req.body;
+        
+        if (!email || !password) {
+            if (!email) {
+                next(ErrorHandler(404, "Email is Missing!"))
+            }
+            if (!password) {
+                next(ErrorHandler(404, "Password is Missing!"))
+            }
+        }
+
+        const GetUser = await User.findOne({
+            email: email
+        })
+
+        if(!GetUser){
+            next(ErrorHandler(404, "Please Provide Valid Credencials!"))
+        }
+
+        const { password: pass, createdAt, updatedAt, __v, ...rest } = GetUser._doc
+
+        res.status(200).json({
+            message: `Welcome Back ${GetUser.name}`,
+            data: rest
+        })
+
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 export default router
