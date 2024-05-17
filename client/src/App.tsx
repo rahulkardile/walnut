c0ae7d92cd6c3aac42cb62ac362220829b970854
Shantanu143 <shantanunirapal@gmail.com>
2024-05-17 08:51:21 +0530

    live project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <div className="bg-[#8faefa]">
+
+
+<Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={""} />
+      </Routes>
+
+      <HomePage />
     </div>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home/HomePage.tsx";
-import Navbar from "./components/navbar/Navbar.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={""} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
